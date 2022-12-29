962c899f19fbe7cd77814542397ddf56f40e8272
sibdip <tskdip@gmail.com>
2022-12-29 16:45:46 +0600

    Tutorial part 2
    https://docs.djangoproject.com/en/4.1/intro/tutorial02/
     set up the database,
     create your first model,
     and get a quick introduction to Django’s automatically-generated
     admin site.

--- wyfda/wyfda/settings.py
+++ wyfda/wyfda/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'wyfda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wyfda_db',
+        'USER': 'db_user',
+        'PASSWORD': 'user_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Almaty'
 
 USE_I18N = True
 
